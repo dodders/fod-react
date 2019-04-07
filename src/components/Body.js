96afc7c9c1ade6e286144277b56a9cabd2dc2587
georged <6376745+dodders@users.noreply.github.com>
2019-04-07 18:56:47 -0400

    full db and table

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,38 +1,67 @@
 import React, { Component } from 'react';
-import getFodMaps from '../data/foods';
-import { Input, List } from 'semantic-ui-react';
+import getFodMaps from '../data/oldfoods';
+import { Input, Table, TransitionablePortal } from 'semantic-ui-react';
 import './body.css';
+import getFoods from '../data/newfoods';
 
 class Body extends Component {
 
     state = {
-        fodmaps: [],
+        foods: getFoods(),
         search: ''
-    }
-
-    componentDidMount() {
-        var fods = getFodMaps();
-        this.setState({
-            fodmaps: fods,
-            search: this.state.search
-        })
     }
 
     doSearch = (event) => {
         this.setState({
-            fodmaps: this.state.fodmaps,
             search: event.target.value
         });
+    }
+
+    getTableRow = (food) => {
+        return (
+            <Table.Row>
+                <Table.Cell>{food.name}</Table.Cell>
+                <Table.Cell>{food.maxuse}</Table.Cell>
+                <Table.Cell>{food.oligols}</Table.Cell>
+                <Table.Cell>{food.fructose}</Table.Cell>
+                <Table.Cell>{food.polyols}</Table.Cell>
+                <Table.Cell>{food.lactose}</Table.Cell>
+                <Table.Cell>{food.comments}</Table.Cell>
+            </Table.Row>
+        );
     }
 
     render() {
         return (
             <div>
                 <div className="header sticky">
-                    <Input id="search" type="text" onChange={this.doSearch} />
+                    <Input id="input" type="text" onChange={this.doSearch} />
                 </div>
                 <div className="body">
-                    <List>
+                    <Table celled>
+                        <Table.Header>
+                            <Table.Row>
+                                <Table.HeaderCell>Food</Table.HeaderCell>
+                                <Table.HeaderCell>Max. Use</Table.HeaderCell>
+                                <Table.HeaderCell>Oligols</Table.HeaderCell>
+                                <Table.HeaderCell>Fructose</Table.HeaderCell>
+                                <Table.HeaderCell>Polyols</Table.HeaderCell>
+                                <Table.HeaderCell>Lactose</Table.HeaderCell>
+                                <Table.HeaderCell>Comments</Table.HeaderCell>
+                            </Table.Row>
+                        </Table.Header>
+
+                        <Table.Body>
+                            {
+                                (this.state.search === '') ?
+                                    this.state.foods.map((food, index) => this.getTableRow(food)) :
+                                    this.state.foods
+                                        .filter(x => x.name.toLowerCase().includes(this.state.search.toLowerCase()))
+                                        .map((food, index) => this.getTableRow(food))
+                            }
+                        </Table.Body>
+                    </Table>
+                    {/* <List>
                             {(this.state.search === '') ?
                                 this.state.fodmaps.map((item, index) => (
                                     <List.Item key={index}>{item}</List.Item>))
@@ -43,7 +72,7 @@ class Body extends Component {
                                         <List.Item key={index}>{item}</List.Item>
                                     ))
                             }
-                    </List>
+                    </List> */}
                 </div>
             </div>
         )
