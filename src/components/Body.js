254d4f1a31273f41fb4398f008922e5298b7339b
georged <6376745+dodders@users.noreply.github.com>
2019-04-07 16:34:41 -0400

    fixed header

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import getFodMaps from '../data/foods';
 import { Input, List } from 'semantic-ui-react';
+import './body.css';
 
 class Body extends Component {
 
@@ -27,20 +28,19 @@ class Body extends Component {
     render() {
         return (
             <div>
-                <div>
+                <div className="header sticky">
                     <Input id="search" type="text" onChange={this.doSearch} />
-                    {/* <input id="search" type="text" onChange={this.doSearch} /> */}
                 </div>
-                <div>
+                <div className="body">
                     <List>
                             {(this.state.search === '') ?
                                 this.state.fodmaps.map((item, index) => (
-                                    <List.Item>{item}</List.Item>))
+                                    <List.Item key={index}>{item}</List.Item>))
                                 :
                                 this.state.fodmaps
-                                    .filter(x => x.includes(this.state.search))
+                                    .filter(x => x.toLowerCase().includes(this.state.search.toLowerCase()))
                                     .map((item, index) => (
-                                        <List.Item>{item}</List.Item>
+                                        <List.Item key={index}>{item}</List.Item>
                                     ))
                             }
                     </List>
